1d0e4d98a55af411cf21dc41430e011f415190ac
Aviv Hasday <avivhasday@gmail.com>
2020-10-08 14:55:38 +0300

    second

---
+++ FirstPage/lang1.js
@@ -0,0 +1,13 @@
+
+
+let English = document.getElementById("En");
+let Hebrew = document.getElementById("He");
+English.addEventListener('click', function () {
+    window.location.href = "https://www.google.com/";
+});
+
+Hebrew.addEventListener('click', function () {
+    console.log("he")
+});
+
+    
